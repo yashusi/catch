94de7b91292fa0ab2f5737c83bdafe077170a7ac
Nirmal Rajendran <nirmal@ad.unc.edu>
2023-02-18 21:26:14 -0500

    Got all information onto page and formatted text

--- src/pages/Donations.js
+++ src/pages/Donations.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import './Donations.css';
+<link href="https://fonts.googleapis.com/css?family=Google+Sans:400,500|Roboto:300,400,500,700|Source+Code+Pro:400,700&display=swap"></link>
 
 export default function Donations() {
   return (
-    <div>Donations</div>
+    <>
+    <h1><b>Donations</b></h1>
+    <h2><b>Past Donation Sites</b></h2>
+    <h3><b>Carolina Institute for Developmental Disabilities</b></h3>
+    <img src="images/Donations/donation1.jpg" alt="Donation to Carolina Institute for Developmental Disabilities"></img>
+    <div class="description">The Carolina Institute for Developmental Disabilities is a comprehensive program for services, research, and training relevant to individuals with developmental disabilities and their families. The Carolina Institute provides a continuum of clinical services from complex, interdisciplinary evaluations on-site to more limited and selected clinical services and training in all 100 counties in North Carolina. The Institute brings together state-of-the-art research and clinical practice to ensure the best possible care for citizens of North Carolina.</div>
+    <div><strong>Total Toys Donated: </strong>20</div>
+    <div><strong>Total Donations: </strong>2</div>
+    <h3><b>UNC Center for Rehabilitative Care</b></h3>
+    <img src="images/Donations/donation2.jpg" alt="Donation to UNC Center for Rehabilitative Care"></img>
+    <div class="description">The mission of the UNC Inpatient Rehabilitation Center is to improve, restore and maintain functional abilities and maximize quality of life in patients with disabilities; educate health care professionals in rehabilitation care and services; and advance rehabilitation research. Rehabilitative care provides persons served with the skills and support necessary to function in an environment with as much independence and choice and as little supervision and restriction as possible. The totality of this care spans the rehabilitation continuum to optimize the functionality and quality of life and prevent and or treat conditions of physically disabled persons.</div>
+    <div><strong>Total Toys Donated: </strong>14</div>
+    <div><strong>Total Donations: </strong>2</div>
+    <h3><b>Atrium Health Levine Children's Hospital</b></h3>
+    <img src="images/Donations/donation3.jpg" alt="Donation to Levine Children's Hospital"></img>
+    <div><strong>Total Toys Donated: </strong>16</div>
+    <div><strong>Total Donations: </strong>2</div>
+    <h3><b>Novant Health</b></h3>
+    <img src="" alt="Donation to Novant Health"></img>
+    <div><strong>Total Toys Donated: </strong>10</div>
+    <div><strong>Total Donations: </strong>1</div>
+    <h3><b>Barton Pond Elementary School</b></h3>
+    <img src="" alt="Donation to Barton Bond Elementary School"></img>
+    <div><strong>Total Toys Donated: </strong>10</div>
+    <div><strong>Total Donations: </strong>1</div>
+    <h3><b>Aversboro Elementary School</b></h3>
+    <img src="" alt="Donation to Aversboro Elementary School"></img>
+    <div><strong>Total Toys Donated: </strong>10</div>
+    <div><strong>Total Donations: </strong>1</div>
+    <h3><b>Past Partners</b></h3>
+    <ul>
+      <li>Autism Society of North Carolina</li>
+      <li>OT for Wake County Public Schools</li>
+    </ul>
+    <p><em>Donation data collection began in Spring 2021.</em></p>
+    </>
   )
 }
+
