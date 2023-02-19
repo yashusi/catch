177483cd986e687c9b4557a9f73e3ca60d1a7269
Andrew Liu <111478660+ahl1u@users.noreply.github.com>
2023-02-18 23:21:38 -0500

    Copied Forms + Titles (#18)
    Co-authored-by: Andrew Liu <andx3w@gmail.com>

--- src/pages/Donations.js
+++ src/pages/Donations.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export default function Donations() {
-  return (
-    <div>Donations</div>
+      const formUrl1 = "https://docs.google.com/forms/d/e/1FAIpQLSfOhVwyU37XieVYEo73C-MyJ1XbY_Hfcy-VB3D31d7F2Tf0Qg/viewform";
+      const formUrl2 = "https://docs.google.com/forms/d/e/1FAIpQLSfACZzhpliXiEolrF0IDf89XFW_RHx7DaSZkDeDLLF618HE1A/viewform";
+
+      return (
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
+            <h1 style={{ marginTop: '25px' }}>Donation Forms</h1>
+            <div style={{ display: 'flex', justifyContent: 'space-between', gap: '50px', width: '80%' }}>
+              <div style={{ width: '50%' }}>
+                <h2 style={{ marginBottom: '15px' }}>Want to Get:</h2>
+                <iframe src={formUrl1} style={{ width: '100%', height: '350px', border: 'none'}} />
+              </div>
+              <div style={{ width: '50%' }}>
+                <h2 style={{ marginBottom: '15px' }}>Want to Give:</h2>
+                <iframe src={formUrl2} style={{ width: '100%', height: '350px', border: 'none'}} />
+              </div>
+            </div>
+          </div>
   )
 }
