2e7674b639a0f78512f96b81111fa71ac740d712
maxxuUNC24 <maxxu@email.unc.edu>
2023-02-26 15:30:18 -0500

    minor changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,33 +1,14 @@
-import React from 'react'
-import React, { useState, useEffect } from 'react';
-import request from 'request';
-import cheerio from 'cheerio';
+import React from 'react';
+
 
 export default function Contact() {
-  const [address, setAddress] = useState('');
-  const [contactForm, setContactForm] = useState('');
 
-  useEffect(() => {
-    const url = 'https://www.catch-inc.com/contact';
 
-    request(url, (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
 
-        const address = $('div.contact-address').text().trim();
-        const contactForm = $('form.contact-form').html();
 
-        setAddress(address);
-        setContactForm(contactForm);
-      }
-    });
-  }, []);
+return (
+  <div> Contact </div>
+)
 
-  return (
-    <div>
-      <h2>Contact</h2>
-      <div>{address}</div>
-      <div dangerouslySetInnerHTML={{ __html: contactForm }} />
-    </div>
-  );
+
 }
