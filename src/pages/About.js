119598c2163fc2508f95d85bf53e2151d009f5e7
Kene Ochuba <89273560+Keneo1@users.noreply.github.com>
2023-02-27 14:32:19 -0500

    Changed About page (#22)
    Co-authored-by: Kene Ochuba <kene.ochuba@unc.edu>
    Co-authored-by: Nithin Sivakumaran <113319591+nsivaku@users.noreply.github.com>

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import FAQ from "./FAQ";
-
+import './About.css';
 
 export default function About(){
 
   return(
     <div className="App">
+      <div>About</div>
+      <div>
+        <div id = 'title'> Our Executive Board </div>
+          <div id = 'table'>
+          <div>
+            <img src = {require('../images/About/thumbnail_image001[92].png')} alt = 'Darci (President)' id = 'images'  />
+            Darci (President)
+          </div>
+          <div>
+            <img src = {require('../images/About/IMG_0557.jpg')} alt = 'Bryce (Vice President)' id = 'images'/>
+            Bryce (Vice President)
+          </div>
+          <div>
+            <img src = {require('../images/About/thumbnail_image2.jpg')} alt = 'Rohan (Treasurer)'id = 'images' />
+            Rohan (Treasurer)
+          </div>
+          <div>
+            <img src = {require('../images/About/thumbnail_image1.jpg')} alt = 'Chris (Secretary)' id = 'images' />
+            Chris (Secretary)
+          </div>
+          <div>
+            <img src = {require('../images/About/thumbnail_IMG_0796.jpg')} alt = 'Jun (Chief Quality Officer)' id = 'images' />
+            Jun (Chief Quality Officer)
+          </div>
+          <div>
+            <img src = {require('../images/About/IMG_5285.jpg')} alt = 'Jacob (Chief Technical Officer)' id = 'images'/>
+            Jacob (Chief Technical Officer)
+          </div>
+          <div>
+            <img src = {require('../images/About/Katie_sHeadshot.jpg')} alt = 'Jacob (Chief Technical Officer)' id = 'images' />
+            Katie (PR Chair)
+          </div>
+        </div>
+      </div>
       <h1>FAQ</h1>
       <FAQ>
         <FAQ.QAItem>
