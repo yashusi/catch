ea6c6e5e17ed9e281390551c7fbe0641901a1130
Better Fork It <69515675+maxxuUNC33@users.noreply.github.com>
2023-03-01 20:06:59 -0500

    Added Back Two Donation Forms

--- src/pages/Donations.js
+++ src/pages/Donations.js
@@ -5,6 +5,8 @@ import './Donations.css';
 export default function Donations() {
 
   const gofundmeform = "https://gofund.me/9dca4d2f";
+  const formUrl1 = "https://docs.google.com/forms/d/e/1FAIpQLSfOhVwyU37XieVYEo73C-MyJ1XbY_Hfcy-VB3D31d7F2Tf0Qg/viewform";
+  const formUrl2 = "https://docs.google.com/forms/d/e/1FAIpQLSfACZzhpliXiEolrF0IDf89XFW_RHx7DaSZkDeDLLF618HE1A/viewform";
   return (
     <>
     <h1><b>Donations</b></h1>
@@ -69,8 +71,24 @@ export default function Donations() {
     </ul>
     <p><em>Donation data collection began in Spring 2021.</em></p>
     </div>
+
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
+        <h1 style={{ marginTop: '25px' }}>Donation Forms</h1>
+        <div style={{ display: 'flex', justifyContent: 'space-between', gap: '50px', width: '80%' }}>
+          <div style={{ width: '50%' }}>
+            <h2 style={{ marginBottom: '15px' }}>Want to Get:</h2>
+            <iframe src={formUrl1} style={{ width: '100%', height: '350px', border: 'none'}} />
+          </div>
+          <div style={{ width: '50%' }}>
+            <h2 style={{ marginBottom: '15px' }}>Want to Give:</h2>
+            <iframe src={formUrl2} style={{ width: '100%', height: '350px', border: 'none'}} />
+          </div>
+        </div>
+      </div>
     </>
 
   )
 }
+
+
 
